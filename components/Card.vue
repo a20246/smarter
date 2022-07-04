9f0d3515fa8ad436fcc5ac930fccf01f23a5becf
yuru <1991xegg@gmail.com>
2022-07-04 22:32:36 +0800

    done

--- components/Card.vue
+++ components/Card.vue
@@ -1,8 +1,54 @@
 <template>
-  <div>card</div>
+  <div class="card">
+    <img class="card-image" :src="info.previewPictureSrc" />
+    <div class="card-body">
+      <div class="title">{{ info.title }}</div>
+      <div
+        class="content lg:max-h-[75px] max-h-[50px] overflow-hidden relative before:content-[''] before:absolute before:w-full before:h-8 lg:before:h-12 before:bg-gradient-to-t before:from-white before:to-transparent before:bottom-0 before:left-0"
+      >
+        {{ info.previewContext }}
+      </div>
+      <a :href="info.url" target="_blank" class="button">立即前往 &#x276F;</a>
+    </div>
+  </div>
 </template>
 
-
 <script>
-export default {};
+export default {
+  props: {
+    info: Object,
+  },
+};
 </script>
+<style scoped>
+.card {
+  width: 323px;
+}
+.card-image {
+  width: 100%;
+  height: 170px;
+}
+.card-body {
+  padding: 18px 5px;
+  text-align: center;
+}
+.title {
+  font-size: 20px;
+}
+.content {
+  font-size: 16px;
+}
+
+.button {
+  font-size: 14px;
+  height: 40px;
+  width: 108px;
+  color: #fff;
+  background: #504e4d;
+  margin: 18px auto;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,38 @@
 <template>
-  <div class="container">
-    <div>
-      <Card />
-    </div>
+  <div class="card-site">
+    <Card v-for="item in data" :key="item.id" :info="item" />
   </div>
 </template>
 
 <script>
 import Card from "~/components/Card.vue";
+import axios from "axios";
 export default {
+  asyncData({ params }) {
+    return axios
+      .get(`https://event.smarter.com.tw/api/spa/bestseller/interview`)
+      .then((res) => {
+        res = res.data;
+        if (res.status === "success") {
+          return { data: res.data };
+        }
+      });
+  },
   components: {
     Card,
   },
   data() {
-    return {
-      apiUrl: "https://event.smarter.com.tw/api/spa/bestseller/interview",
-    };
+    return {};
   },
+  mounted() {},
 };
 </script>
 
 <style scoped>
+.card-site {
+  margin: 5rem auto;
+  width: 1029px;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
