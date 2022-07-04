ce69b00b9eeb92807dd618ece46a5178fa986bd9
Yuru <73826421+a20246@users.noreply.github.com>
2022-07-04 18:12:59 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div>
+      <Card />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "~/components/Card.vue";
+export default {
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      apiUrl: "https://event.smarter.com.tw/api/spa/bestseller/interview",
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
